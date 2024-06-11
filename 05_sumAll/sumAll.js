8ba077974f59f44bc282714884e56c5e73f05d05
mpreams <mreams97@gmail.com>
2024-06-11 13:38:09 -0400

    Complete first six exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(low, high) {
+    let sum = 0;
+    if (low > high) {
+        let n = low;
+        low = high;
+        high = n;
+    }
+    if (low < 0 || high < 0 || typeof low != 'number' || typeof high != 'number') {
+        return "ERROR";
+    }
+    for (let i = low; i <= high; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
