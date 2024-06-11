d43319ee1e1df8df325e4f1e3142e74737596bf6
mpreams <mreams97@gmail.com>
2024-06-11 02:36:28 -0400

    Complete first four exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const forwardArray = string.split("");
+    const reverseArray = forwardArray.reverse();
+    let output = reverseArray.join('');
+    return output;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    const output = [];
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            output.push(item);
+        }
+    }); 
+    return output;
 };
 
 // Do not edit below this line
