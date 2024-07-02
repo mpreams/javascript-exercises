51f778492e8c4a8051b7c09ce041e5937381b809
mpreams <mreams97@gmail.com>
2024-07-02 00:18:39 -0400

    Complete the last of the exercises,  exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    let agesObj = [];
+    let ages = [];
+    
+    people.forEach(person => {
+        if (!person.yearOfDeath) { 
+            agesObj.push({name: person.name, age: currentYear - person.yearOfBirth});
+            ages.push(currentYear - person.yearOfBirth);
+        } else {
+            agesObj.push({name: person.name, age: person.yearOfDeath - person.yearOfBirth});
+            ages.push(person.yearOfDeath - person.yearOfBirth);
+        };
+    });
 
+    let oldestAge = Math.max(...ages);
+    let oldestName = agesObj.find(person => person.age == oldestAge).name;
+    return people.find(person => person.name == oldestName);
 };
 
 // Do not edit below this line
