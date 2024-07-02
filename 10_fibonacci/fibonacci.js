6626777c5c8945c2bca4d69a1794ebca6bbd1f64
mpreams <mreams97@gmail.com>
2024-07-01 23:33:21 -0400

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) return "OOPS";
+    if (n == 0) return 0;
+    let array = [1, 1];
+    for (let i = 2; i < n; i++) {
+        array.push(array[i - 1] + array[i - 2]);
+    }
+    return array[n - 1];
 };
 
 // Do not edit below this line
