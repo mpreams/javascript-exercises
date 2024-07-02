1f588b402a93350c1973ad531eec0fbd14b6ae5e
mpreams <mreams97@gmail.com>
2024-07-01 23:14:02 -0400

    Complete all but one test for exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    let noSpaceText = text.toLowerCase().split(' ').join('');
+    let arr = [];
+    for (let i = 0; i < noSpaceText.length; i++) {
+        if (noSpaceText[i].match(/[a-z]/i)) {
+            arr.push(noSpaceText[i]);
+        }
+    }
+    arrReversed = arr.slice().reverse();
+    return arr.join('') === arrReversed.join('');
 };
 
 // Do not edit below this line
